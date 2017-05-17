94e7b3a59dd06ed822dfb3de56022327b1ada965
telicr <Roman.Telichkin@dell.com>
2017-05-17 11:13:08 +0300

    Add test for creation user in db

--- tests/user_test.py
+++ tests/user_test.py
@@ -4,6 +4,7 @@ from flask_testing import TestCase
 
 import config
 import application
+from application.models import User
 
 
 class UserCreationPositiveTestCase(TestCase):
@@ -18,9 +19,13 @@ class UserCreationPositiveTestCase(TestCase):
             "password": "SuperPWD!"
         }
 
-    def test_should_return_201_on_creation(self):
+    def test_should_return_201(self):
         response = self.get_create_response(data=self.user_data)
         assert response.status_code == 201
+
+    def test_should_create_user_in_db(self):
+        self.get_create_response(data=self.user_data)
+        assert User.query.filter_by(username=self.user_data["username"])
 
     def get_create_response(self, data):
         return self.client.post(self.uri,
@@ -28,4 +33,5 @@ class UserCreationPositiveTestCase(TestCase):
                                 content_type="application/json")
 
     def tearDown(self):
+        application.db.session.remove()
         application.db.drop_all()
