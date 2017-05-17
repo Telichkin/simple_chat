c4ab59976ea1f4c758a9e8cdd61e2e8158da49c2
telicr <Roman.Telichkin@dell.com>
2017-05-17 18:56:13 +0300

    Refactor chat tests

--- tests/chat_test.py
+++ tests/chat_test.py
@@ -5,22 +5,26 @@ from application import socket_io
 class AuthBase(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.user_data = {
-            "username": "Telichkin",
-            "password": "SuperPWD!"
-        }
-        self.socket_client = socket_io.test_client(self.app)
-        self.socket_client.get_received()
+        self.user_data_list = [
+            {"username": "Telichkin", "password": "SuperPWD!"},
+            {"username": "Some", "password": "Other"},
+            {"username": "John", "password": "Doe"}
+        ]
+        self.client_list = [socket_io.test_client(self.app) for _ in self.user_data_list]
+        self.token_list = [self.json_post("/users/", user_data).json["token"]
+                           for user_data in self.user_data_list]
 
     # SocketTO().test_client works incorrectly, it can't receive messages when more than 1
     # tests with it is written.
     def test_auth(self):
-        response = self.json_post("/users/", self.user_data)
-        self.socket_client.emit("auth", {"token": response.json["token"]})
-        assert self.socket_client.get_received()[0]["args"] == "authenticated"
+        self.client_list[0] = socket_io.test_client(self.app)
+        self.client_list[0].get_received()
 
-        self.socket_client.emit("auth", {"token": response.json["token"][:-3] + "qwe"})
-        assert self.socket_client.get_received()[0]["args"] == "authentication error"
+        self.client_list[0].emit("auth", {"token": self.token_list[0]})
+        assert self.client_list[0].get_received()[0]["args"] == "authenticated"
 
-        self.socket_client.emit("auth", {})
-        assert self.socket_client.get_received()[0]["args"] == "Token is needed"
+        self.client_list[0].emit("auth", {"token": self.token_list[0][:-3] + "qwe"})
+        assert self.client_list[0].get_received()[0]["args"] == "authentication error"
+
+        self.client_list[0].emit("auth", {})
+        assert self.client_list[0].get_received()[0]["args"] == "Token is needed"
