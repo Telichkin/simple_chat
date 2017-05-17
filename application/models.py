24fe608fa759846b4d27940a0b46cc37614afb3f
telicr <Roman.Telichkin@dell.com>
2017-05-17 11:04:27 +0300

    Add user model

---
+++ application/models.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash
+
+from application import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Write-only field")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
