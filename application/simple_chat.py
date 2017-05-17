b6f26101e94292795af4f344d2d54160517cac52
telicr <Roman.Telichkin@dell.com>
2017-05-17 11:04:45 +0300

    Add endpoint for user creation

--- application/simple_chat.py
+++ application/simple_chat.py
@@ -1,9 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
+
+from application import db
+from application.models import User
 
 
-root = Blueprint('root', __name__)
+root = Blueprint("root", __name__)
 
 
-@root.route('/')
+@root.route("/")
 def hello_world():
-    return 'Hello World!'
+    return "Hello World!"
+
+
+@root.route("/users/", methods=["POST"])
+def create_user():
+    user_data = request.get_json()
+    user = User(
+        username=user_data["username"],
+        password=user_data["password"]
+    )
+    db.session.add(user)
+    db.session.commit()
+    return "OK", 201
