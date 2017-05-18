3468a5f930606a11a278e15bdbd9d81aed3ad838
telicr <Roman.Telichkin@dell.com>
2017-05-19 01:13:49 +0300

    Add CLI to manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,22 @@
+"""
+Usage: 
+    manage.py start (--development | --production) [--host=<host>] [--port=<port>]
+
+Examples:
+    python manage.py start --production --host=localhost --port=8080
+"""
+from docopt import docopt
+
 import config
 import application
 
 
 if __name__ == '__main__':
-    app = application.create(config.DevelopmentConfig)
+    arguments = docopt(__doc__)
+    if arguments["--development"]:
+        app = application.create(config.DevelopmentConfig)
+    else:
+        app = application.create(config.ProductionConfig)
     app.app_context().push()
     application.db.create_all()
-    application.socket_io.run(app)
+    application.socket_io.run(app, host=arguments["--host"], port=arguments["--port"])
